25127fa8206deea690aee0a00ec8ef3000f89291
Blossom <saphashb@gmail.com>
2025-04-22 21:39:09 -0400

    projects

---
+++ src/utils.js
@@ -0,0 +1,66 @@
+export const UtilsProjects = [
+    {
+        title: 'Distraction Blocker - Chrome extension',
+        description: "Distraction Blocker is a Chrome extension designed to help you stay focused. It replaces YouTube thumbnails and video titles with a simple black image and a reminder to focus, allowing you to regain control of your browsing experience.",
+        liveLink: "",
+        github: "https://github.com/blossom-babs/Distraction-Blocker",
+        stack: "HTML, CSS, Javascript"
+    },
+    {
+        title: 'js-ord NPM package',
+        description: "A lightweight utility library for performing common character operations in JavaScript with cleaner, more readable syntax. It simplifies working with ASCII codes and alphanumeric characters.",
+        liveLink: "https://www.npmjs.com/package/js-ord",
+        github: "https://github.com/blossom-babs/js-ord",
+        stack: "Javascript",
+        imgRef: "https://i.imgur.com/LMfOeZ0.png"
+    },
+   
+    {
+        title: "Tsavar's portfolio",
+        description: "The site serves as a digital portfolio for a product designer, helping them showcase their work, values, present case studies, highlight key projects, and express their design philosophy",
+        liveLink: "https://www.shatermt.com/",
+        github: "https://github.com/blossom-babs/shater-port",
+        stack: "Next.js, Typescript, Vercel",
+        imgRef: "https://i.imgur.com/a3jIFO1.png"
+    },
+    {
+        title: 'csv-to-json API',
+        description: "A simple API that allows users to convert CSV files into clean, structured JSON format. Streamlines data transformation.",
+        liveLink: "",
+        github: "https://github.com/blossom-babs/exercise-csv-to-json",
+        stack: "HTML, CSS, Javascript",
+        imgRef: "https://i.imgur.com/xvDbcUe.png"
+    },
+    {
+        title: 'rubix API',
+        description: "An API for resizing image to thumbanil sized",
+        liveLink: "",
+        github: "https://github.com/blossom-babs/exercise-rubix",
+        stack: "Typescript, Node/Express",
+        imgRef: "https://i.imgur.com/sVYny73.png"
+    },
+    {
+        title: 'Landing Page',
+        description: "This is a responsive clone of the Stutern landing page, built with pure HTML and CSS. The project replicates the design and structure of the original website focusing on layout, responsiveness, and visual design.",
+        liveLink: "https://blossom-babs.github.io/stutern-lp-clone/",
+        github: "https://github.com/blossom-babs/stutern-lp-clone",
+        stack: "Javascript, HTML, SCSS",
+        imgRef: "https://i.imgur.com/AHpE9Io.png"
+    },
+    {
+        title: 'Task tracker',
+        description: "A classic react-based to-do list to Keep track of your daily tasks, activities and goals",
+        liveLink: "https://blossom-keep-note.netlify.app/",
+        github: "https://github.com/blossom-babs/Task-Tracker",
+        stack: "React, Javascript",
+        imgRef: "https://i.imgur.com/8VgHsnS.png"
+    },
+    {
+        title: 'GitHack',
+        description: "Built using GitHub API integration, GitHack allows users to search for summarized details on any GitHub user.",
+        liveLink: "https://githack.netlify.app/",
+        github: "https://github.com/blossom-babs/Git-Hack",
+        stack: "Typescript, HTML, SCSS",
+        imgRef: "https://i.imgur.com/7x9TKhZ.png"
+    },
+]
